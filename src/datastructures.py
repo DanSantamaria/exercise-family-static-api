133e232212008379585e1eb9be74b07898eb96aa
Daniel Santamaría <81576160+DanSantamaria@users.noreply.github.com>
2026-03-24 17:06:46 +0100

    Implement member management methods in FamilyStructure

--- src/datastructures.py
+++ src/datastructures.py
@@ -1,45 +1,53 @@
+
 """
-Update this file to implement the following already declared methods:
+update this file to implement the following already declared methods:
 - add_member: Should add a member to the self._members list
 - delete_member: Should delete a member from the self._members list
+- update_member: Should update a member from the self._members list
 - get_member: Should return a member from the self._members list
 """
+from random import randint
 
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-        self._next_id = 1
-        self._members = [
-            {
-                "id": self._generate_id(),
-                "first_name": "John",
-                "last_name": last_name,
-                "age": 33,
-                "lucky_numbers": [7, 13, 22]
-            }
-        ]
 
-    # This method generates a unique incremental ID
-    def _generate_id(self):
-        generated_id = self._next_id
-        self._next_id += 1
-        return generated_id
+        # example list of members
+        self._members = []
+        self._next_id = 1
+
+        # add initial members
+        self.add_member({"first_name": "John", "age": 33, "lucky_numbers": [7, 13, 22]})
+        self.add_member({"first_name": "Jane", "age": 35, "lucky_numbers": [10, 14, 3]})
+        self.add_member({"first_name": "Jimmy", "age": 5, "lucky_numbers": [1]})
+
+    # read-only: Use this method to generate random members ID's when adding members into the list
+    def _generateId(self):
+        return randint(0, 99999999)
 
     def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
+        # fill this method and update the return
+        new_id = self._generateId()
+        member['id'] = new_id
+        member['last_name'] = self.last_name
+        self._members.append(member)
+        return member
         pass
 
     def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        pass
+        for i, m in enumerate(self._members):
+            if m['id'] == id:
+                self._members.pop(i)
+                return True
+        return False
 
     def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        pass
+        # fill this method and update the return
+        for member in self._members:
+            if member['id'] == id:
+                return member
+        return None
 
-    # This method is done, it returns a list with all the family members
+    # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
